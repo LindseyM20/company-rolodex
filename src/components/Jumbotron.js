70033e85a18922d28e3cecb0040d20d0cda7335e
LindseyM20 <lindsey.bordner@gmail.com>
2020-11-19 22:33:20 -0600

    name change

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -5,7 +5,7 @@ function Jumbotron() {
   return (
     <div className="jumbotron jumbotron-fluid">
       <div className="container">
-        <h1 className="display-4">Employee Rolodex</h1>
+        <h1 className="display-4">Company Rolodex</h1>
         <p className="lead">scroll  ||  search by name or phone #  ||  sort by last name</p>
       </div>
     </div>
